81798ab7e7eec6d641d5f443f9a0322666227102
sox1d <yaskodanil@gmail.com>
2025-01-20 19:45:56 +0300

    First Danya commit

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def main():
-    print('My first git program')
+import pygame
+import pygame_gui
 
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+manager = pygame_gui.UIManager((800, 600))
 
-if __name__ == '__main__':
-    main()
+button = pygame_gui.elements.UIButton(
+    relative_rect=pygame.Rect((350, 275), (100, 50)),
+    text="Нажми меня",
+    manager=manager
+)
+
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    time_delta = clock.tick(60) / 1000.0
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        manager.process_events(event)
+
+    manager.update(time_delta)
+    screen.fill((0, 0, 0))
+    manager.draw_ui(screen)
+    pygame.display.update()
